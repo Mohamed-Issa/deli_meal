c90193a8190a0e208f63050c04d3788d5a202161
mohamed-issa <mohamed.hamdy.issa@gmail.com>
2020-04-06 18:07:54 +0200

    displaying recipe items & using network images

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+import '../dammy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+//  final String categoryID;
+//  final String categoryTitle;
+//
+//  CategoryMealsScreen({this.categoryID,this.categoryTitle});
+  @override
+  Widget build(BuildContext context) {
+    final routeArg =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArg['title'];
+    final categoryID = routeArg['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryID);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
